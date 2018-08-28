9c8aa41e44ce6ac214fb13163d65c7de60ae22c2
starlit010 <starlit010@hotmail.com>
2018-08-28 18:54:09 +0800

    修改配置

---
+++ src/main/java/com/grayliu/alg/string/LCS.java
@@ -0,0 +1,32 @@
+package com.grayliu.alg.string;
+
+/**
+ * Created by liuhui-ds9 on 2018/8/28.
+ *
+ * longgest common string
+ *
+ * 求最长不连续子串
+ *
+ * 比较二维数组法和哈希法
+ *
+ * */
+public class LCS {
+
+    public static String A = "BDCABA";
+    public static String B = "ABCBDAB";
+
+    public void hashSolution(){
+
+    }
+
+    public void dynamicSolution(){
+
+    }
+
+    public static void main(String...args){
+
+    }
+
+
+
+}
---
+++ src/main/java/com/grayliu/alg/string/LIS.java
@@ -0,0 +1,20 @@
+package com.grayliu.alg.string;
+
+/**
+ * Created by liuhui-ds9 on 2018/8/28.
+ *
+ * longest increase string
+ *
+ * 求最长连续子字符串
+ *
+ * 也有叫DP的
+ */
+public class LIS {
+
+
+
+
+
+
+
+}
