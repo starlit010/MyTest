e501b903abf807e49d189b0159592c81a0cb879d
starlit010 <starlit010@hotmail.com>
2018-08-30 17:08:26 +0800

    jdk8

---
+++ src/main/java/com/grayliu/jdk/jdk8/FunctionTest.java
@@ -0,0 +1,27 @@
+package com.grayliu.jdk.jdk8;
+
+import java.util.function.Function;
+
+/**
+ * Created by liuhui-ds9 on 2018/8/30.
+ *
+ * 调用function的默认构造方法apply，注意function的范型类型定义
+ *
+ * 这个接口的作用主要是定义一个对象的转换
+ */
+public class FunctionTest {
+
+    public void test(){
+
+        Function<String,String> function = new Function(){
+
+            @Override
+            public Object apply(Object o) {
+                return null;
+            }
+        };
+
+    }
+
+
+}
--- src/main/java/com/grayliu/jdk/lang/ArraysTest.java
+++ src/main/java/com/grayliu/jdk/lang/ArraysTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Created by liuhui-ds9 on 2018/8/23.
  *
- * 1）Arrays.asList 不接受基本数据类型
+ * 1）Arrays.asList 不接受基本数据类型（int[]，long[]，char[]，这些类型会被认为是一个array对象）
  *
  * 2）asList后再iterator不能调用remove方法（asList的对象是Arrays自定义的没有定义remove方法，而iterator的remove的时候会调用实例对象的remove方法，所以就会报unsupportoperationexception）
  *
