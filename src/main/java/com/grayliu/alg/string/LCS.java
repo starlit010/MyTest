27236a4d15fb4f557d2cb74425adbdab0058e4ba
starlit010 <starlit010@hotmail.com>
2018-09-06 10:31:26 +0800

    lcs

--- src/main/java/com/grayliu/alg/string/LCS.java
+++ src/main/java/com/grayliu/alg/string/LCS.java
@@ -3,27 +3,70 @@ package com.grayliu.alg.string;
 /**
  * Created by liuhui-ds9 on 2018/8/28.
  *
- * longgest common string
+ * longgest common sequence
  *
- * 求最长不连续子串
+ * 求最长公共子序列
  *
  * 比较二维数组法和哈希法
  *
  * */
 public class LCS {
 
-    public static String A = "BDCABA";
-    public static String B = "ABCBDAB";
+    //expact blog
+    public static String A = "cnblogs";
+    public static String B = "belong";
 
-    public void hashSolution(){
+    void solution(){
+        char[] arrayA = A.toCharArray();
+        char[] arrayB = B.toCharArray();
 
-    }
+        StringBuilder maxSeq = new StringBuilder();
+        StringBuilder tempSeq = new StringBuilder();
 
-    public void dynamicSolution(){
+        int tempX = 0,tempY = 0;
+        boolean isContinue =false;
 
+        outter:for(int x = 0 ; x < arrayA.length ; x++){
+
+            inner:for(; tempY < arrayB.length ; tempY++){
+
+                if(arrayB[tempY] == arrayA[x]){
+                    tempSeq.append(arrayB[tempY]);
+
+                    if(tempY == arrayB.length - 1){
+                        tempY = 0;
+                    }
+                    if(!isContinue){
+                        tempX = x;
+                        isContinue = true;
+                    }
+                    if(tempY != arrayB.length - 1){
+                        continue outter;
+                    }
+                }
+
+                if(tempY == arrayB.length - 1){
+                    if(tempSeq.length() > maxSeq.length()){
+                        maxSeq = tempSeq;
+                        tempSeq = new StringBuilder();
+                    }
+                    if(tempX != 0 && x != arrayA.length - 1){
+                        x = tempX;
+                    }
+                    tempY = 0;
+                    isContinue = false;
+                    continue outter;
+                }
+            }
+        }
+
+        System.out.println(maxSeq);
     }
 
     public static void main(String...args){
+
+        LCS lcs = new LCS();
+        lcs.solution();
 
     }
 
--- src/main/java/com/grayliu/alg/string/LIS.java
+++ src/main/java/com/grayliu/alg/string/LIS.java
@@ -3,18 +3,139 @@ package com.grayliu.alg.string;
 /**
  * Created by liuhui-ds9 on 2018/8/28.
  *
- * longest increase string
+ * longest increase sequence
  *
  * 求最长连续子字符串
+ *
+ * 只要不连续就保存，进行下一次比较
  *
  * 也有叫DP的
  */
 public class LIS {
 
+    //expact lo
+    static String A = "cnblogs";
+    static String B = "belong";
+
+    char[] arrayA = A.toCharArray();
+    char[] arrayB = B.toCharArray();
+
+    StringBuilder tempSeq = new StringBuilder();
+    StringBuilder maxSeq = new StringBuilder();
+
+    public void solution(){
+
+        boolean isContinue =false;
+
+        int i = 0, j = 0;
+        int tempI = 0,tempJ = 0;
+
+        outter: for(; i < arrayA.length ; i++){
+
+            inner: for(; j < arrayB.length ; j++){
+
+                if(arrayA[i] == arrayB[j]){
+                    tempSeq.append(arrayB[j]);
+                    if(!isContinue){
+                        tempI = i;
+                        tempJ = j;
+                        isContinue = true;
+                        j++;
+                    }
+                    if(j == arrayB.length - 1){
+                        if(tempSeq.length() > maxSeq.length()){
+                            maxSeq = tempSeq;
+                            tempSeq = new StringBuilder();
+                        }
+                        if(tempI > 0){
+                            i = tempI;
+                            j = tempJ;
+                            tempI = 0;
+                            tempJ = 0;
+                        }
+                        isContinue = false;
+                    }
+                    continue outter;
+                }else{
+                    if(isContinue){
+                        if(tempSeq.length() > maxSeq.length()){
+                            maxSeq = tempSeq;
+                        }
+                        if(tempI > 0){
+                            i = tempI;
+                            tempI = 0;
+                            tempJ = 0;
+                        }
+                        tempSeq = new StringBuilder();
+
+                    }else if(j == arrayB.length - 1){
+                        if(tempI > 0){
+                            i = tempI;
+                            tempI = 0;
+                            tempJ = 0;
+                        }
+                    }
+                    j = 0;
+                    isContinue = false;
+                    continue outter;
+                }
+            }
+
+        }
+
+        System.out.println(maxSeq);
+    }
+
+    void solutionII(){
+
+        int i = 0 ,j = 0;
+        int tempI = 0 , tempJ = 0;
+        boolean isContinue = false;
+
+        while(true){
+
+            if( i >= arrayA.length || j >= arrayB.length){
+                break;
+            }
+
+            if(arrayA[i] == arrayB[j]){
+                if(!isContinue){
+                    isContinue = true;
+                    tempI = i;
+                }
+                tempSeq.append(arrayB[j]);
+                i++;
+                j++;
+            }else{
+                if(isContinue){
+                    if(tempSeq.length() > maxSeq.length()){
+                        maxSeq = tempSeq;
+                    }
+                    tempSeq = new StringBuilder();
+                    i = tempI + 1;
+                    j = 0;
+                    isContinue = false;
+                }else{
+                    if(j == arrayB.length - 1){
+                        i = tempI + 1;
+                        j = 0;
+                        isContinue = false;
+                    }else{
+                        j++;
+                    }
+                }
+            }
+
+        }
+
+        System.out.println(maxSeq);
 
 
+    }
 
-
-
+    public static void main(String...args){
+        LIS lis = new LIS();
+        lis.solutionII();
+    }
 
 }
