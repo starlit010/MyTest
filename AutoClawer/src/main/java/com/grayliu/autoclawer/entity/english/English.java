93946f409168f4056ae1bc62798b7f9fe8affc06
liuhui67@wanda.cn <liuhui67@wanda.cn>
2018-12-09 22:19:10 +0800

    english commit

---
+++ AutoClawer/src/main/java/com/grayliu/autoclawer/entity/english/English.java
@@ -0,0 +1,28 @@
+package com.grayliu.autoclawer.entity.english;
+
+import lombok.Data;
+
+/**
+ * Created by liuhui-ds9 on 2018/11/21.
+ */
+@Data
+public class English {
+
+    Integer id;
+
+    Integer level;
+
+    String property;
+
+    String english;
+
+    String chinese;
+
+    String symbol;
+
+    byte[] prononce;
+
+    String sentence;
+
+
+}
